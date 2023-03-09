2a91865d34bc2e21b44041fd478d84d1b1c1ce50
Aleh <alehkhvasko@yahoo.com>
2023-03-09 13:48:23 -0600

    Initial commit

---
+++ src/main/java/com/cognizant/mstolls/MsTollsApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.mstolls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTollsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTollsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cognizant/mstolls/MsTollsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.mstolls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTollsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
